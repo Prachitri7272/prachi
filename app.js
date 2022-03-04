16ef6aa85ffecf0bc89158cf906c24ec1814bea1
Prachitri7272 <prachitripathi243@gmail.com>
2022-03-04 20:26:43 +0530

    project created

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-//db.sequelize.sync();
+db.sequelize.sync();
 //db.sequelize.sync({force:true});
 
 app.use('/employees', employeesRouter);
